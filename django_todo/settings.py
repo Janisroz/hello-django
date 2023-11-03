b40120e54d1a84028136c041efb77c0970d31b70
Janisroz <janisrozkalejs@gmail.com>
2023-11-03 21:40:23 +0000

    Fix deployment issue

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -23,13 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    'SECRET_KEY', 'django-insecure-38e^m-6fhl0tt&!4^b)bh$7h)gadjr5n_p5&x6kl7bf8q43)hf')
+SECRET_KEY = 'django-insecure-38e^m-6fhl0tt&!4^b)bh$7h)gadjr5n_p5&x6kl7bf8q43)hf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ,get('HEROKU_HOSTNAME')]
+ALLOWED_HOSTS = ['8000-janisroz-hello-django-qer7nfga07.us2.codeanyapp.com']
 
 
 # Application definition
