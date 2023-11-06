f258b037b9371a2ffa71df2a2978b12124019c29
Janisroz <janisrozkalejs@gmail.com>
2023-11-06 18:43:43 +0000

    Update deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -23,12 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-38e^m-6fhl0tt&!4^b)bh$7h)gadjr5n_p5&x6kl7bf8q43)hf'
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY', 'django-insecure-38e ^ m-6fhl0tt &!4 ^ b)bh$7h)gadjr5n_p5 & x6kl7bf8q43)hf')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['todo-app-janisroz-cb0131cf0808.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('todo-app-janisroz-cb0131cf0808.herokuapp.com')]
 
 
 # Application definition
@@ -84,7 +85,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 #     }
 # }
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('postgres://vhelqayo:Y7EzclJh-mghs3cHPT2CNyfZZVXk8nV5@flora.db.elephantsql.com/vhelqayo'))
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 # Password validation
